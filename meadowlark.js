78022c962311d7a0e44304970cd01a219700a012
Yuan Xue <yuanxue68@gmail.com>
2015-03-30 01:44:31 -0400

    move fortune into a module

--- meadowlark.js
+++ meadowlark.js
@@ -1,18 +1,14 @@
 var express = require('express');
-var app = express();
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+
+//custom modles
+var fortune=require('./lib/fortune.js');
+//app initialization
+var app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname+'/public'));
-
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
 
 //route to index
 app.get('/',function(req,res){
@@ -20,10 +16,8 @@ app.get('/',function(req,res){
 });
 //route to about
 app.get('/about',function(req,res){
-	var randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)];
-	res.render('about',{fortune:randomFortune});
+	res.render('about',{fortune:fortune.getFortune});
 })
-
 
 // custom 404 page
 app.use(function(req, res){
