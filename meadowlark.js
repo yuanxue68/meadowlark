1a7117c3c33077119388fa3680ac009db6935160
Yuan Xue <yuanxue68@gmail.com>
2015-03-30 01:26:36 -0400

    add templates

--- meadowlark.js
+++ meadowlark.js
@@ -1,18 +1,40 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname+'/public'));
+
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
+
+//route to index
+app.get('/',function(req,res){
+	res.render('home');
+});
+//route to about
+app.get('/about',function(req,res){
+	var randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)];
+	res.render('about',{fortune:randomFortune});
+})
+
+
 // custom 404 page
 app.use(function(req, res){
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 // custom 500 page
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 app.listen(app.get('port'), function(){
 	console.log( 'Express started on http://localhost:' +
