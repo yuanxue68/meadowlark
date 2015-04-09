520f5fe8e40a08bc09d84f0110cd219a42d2cd48
Yuan Xue <yuanxue68@gmail.com>
2015-04-09 01:32:18 -0400

    added mongoose

--- credentials.js
+++ credentials.js
@@ -3,5 +3,13 @@ module.exports={
 	gmail:{
 		user: 'youremail@email.com',
 		password: '123456',
-	}
+	},
+	mongo: {
+		development: {
+			connectionString: 'your_dev_connection_string',
+		},
+		production: {
+			connectionString: 'your_production_connection_string',
+		},
+	},
 };
