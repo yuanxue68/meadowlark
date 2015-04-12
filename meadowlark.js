c979964f9bc142eda2a606dd540010bf1a9f5885
Yuan Xue <yuanxue68@gmail.com>
2015-04-11 21:01:55 -0400

    Revert "added cluster to start mutiple app"
    This reverts commit c5bde248a9d80205ae0d770da71582b382d61d01.

--- meadowlark.js
+++ meadowlark.js
@@ -30,12 +30,6 @@ app.use(require('express-session')());
 app.use(cartValidation.checkWaivers);
 app.use(cartValidation.checkGuestCounts);
 
-app.use(function(req,res,next){
-var cluster = require('cluster');
-if(cluster.isWorker) console.log('Worker %d received request',
-cluster.worker.id);
-});
-
 var mailTransport=nodemailer.createTransport('SMTP',{
 	service:"Gmail",
 	auth: {
@@ -175,20 +169,4 @@ function getWeatherData(){
 		},
 		],
 	};
-}
-
-function startServer() {
-	http.createServer(app).listen(app.get('port'), function(){
-		console.log( 'Express started in ' + app.get('env') +
-		' mode on http://localhost:' + app.get('port') +
-		'; press Ctrl-C to terminate.' );
-	});
-}
-
-if(require.main === module){
-    // application run directly; start app server
-    startServer();
-} else {
-    // application imported as a module via "require": export function to create server
-    module.exports = startServer;
 }
